b357f309edc60b9ad816cb56cf4ff1cbbe1425a3
Gary Luo <galu@microsoft.com>
2020-06-27 16:03:35 -0700

    Modified some data, potentially need to do more data cleanup

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
-mvp_data = pd.read_csv(r'data/MVP.csv')
-player_data = pd.read_csv(r'data/Seasons_Stats.csv')
-
-print(mvp_data)
-print(player_data)
+player_data = pd.read_csv(r'data/player_data.csv')
 
 def get_player_data(player, year):
-    return player_data.loc[(player_data["Player"] == player) & (player_data["Year"] == year)]
+    all_data = player_data.loc[(player_data["Player"] == player) & (player_data["Year"] == year)]
+    if (all_data.shape[0] != 1):
+        return all_data.loc[(all_data["Tm"] == "TOT")]
+    return all_data
 
 def get_mvps():
-    return mvp_data.loc[mvp_data["MVP"] == 1]
+    return player_data.loc[player_data["MVP"] == 1]
 
-print(get_player_data("James Harden", 2010))
-print(get_mvps())
+def main():
+    print(get_player_data("Dennis Awtrey", 1979))
+
+if __name__ == "__main__":
+    main()
